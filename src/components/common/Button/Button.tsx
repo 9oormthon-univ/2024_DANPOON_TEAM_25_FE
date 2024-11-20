0dc728399b877e7d4494081a808b741ac6377b98
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-21 00:08:30 +0900

    [feat] 네이버 및 구글 로그인 버튼 추가 구현

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends BaseProps {
 }
 
 interface LoginButtonProps extends ButtonProps {
-  type: 'kakao' | 'email';
+  type: 'kakao' | 'naver' | 'google' | 'email';
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -48,16 +48,21 @@ export const LoginButton: React.FC<LoginButtonProps> = ({
   children,
   type,
   ...props
-}) => (
-  <Button
-    {...props}
-    className={`text-black py-3 gap-x-2 max-w-[325px] w-full px-10  sm:w-auto rounded-md cursor-pointer text-sm bg-[#FEE500] ${props.className}`}
-  >
-    {type === 'kakao' ? (
-      <img src="/kakao.svg" alt="Kakao Icon" />
-    ) : (
-      <Mail className="w-4 h-4" />
-    )}
-    {children}
-  </Button>
-);
+}) => {
+  const IconMapping: Record<string, JSX.Element> = {
+    kakao: <img src="/kakao.svg" alt="Kakao Icon" />,
+    naver: <img src="/naver.svg" alt="Naver Icon" />,
+    google: <img src="/google.svg" alt="Naver Icon" />,
+    email: <Mail className="w-4 h-4" />,
+  };
+
+  return (
+    <Button
+      {...props}
+      className={`text-black py-3 gap-x-2 max-w-[325px] w-full px-10 sm:w-auto rounded-md cursor-pointer text-sm  ${props.className}`}
+    >
+      {IconMapping[type] || null}
+      {children}
+    </Button>
+  );
+};
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -18,7 +18,27 @@ const Login = () => {
         </section>
 
         <section className="items-center justify-center flex flex-col space-y-4">
-          <LoginButton children="카카오로 3초만에 시작하기" type="kakao" />
+          <LoginButton
+            children="카카오로 3초만에 시작하기"
+            type="kakao"
+            className="bg-[#FEE500]"
+          />
+          <LoginButton
+            children="네이버로 3초만에 시작하기"
+            type="naver"
+            className="bg-[#03C75A]"
+          />
+          <LoginButton
+            children="구글로 3초만에 시작하기"
+            type="google"
+            className="bg-white border-[#E7E7E7] border px-11"
+          />
+          <div className="flex flex-row justify-center items-center gap-x-4">
+            <div className="border-b border-gray-300 w-24" />
+            <p className="text-sm text-gray-500">또는</p>
+            <div className="border-b border-gray-300 w-24" />
+          </div>
+
           <LoginButton
             children="이메일로 시작하기"
             type="email"
