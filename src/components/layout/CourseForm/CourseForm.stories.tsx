cea95c652885195459fc71a9fd40008666e15140
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-21 19:30:18 +0900

    [feat] CourseForm 컴포넌트 및 스토리북 구현

---
+++ src/components/layout/CourseForm/CourseForm.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { CourseForm } from '@/components/layout/CourseForm/CourseForm';
+
+const meta: Meta<typeof CourseForm> = {
+  title: 'Components/CourseForm',
+  component: CourseForm,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+const Template: StoryFn<typeof CourseForm> = () => <CourseForm />;
+
+export const Default = Template.bind({});
