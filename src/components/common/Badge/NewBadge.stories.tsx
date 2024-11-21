fd1ca9ae6f814f90dda6ff0d9d0185af7b7a144a
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-22 03:43:43 +0900

    [refactor] NewBadge 컴포넌트 및 스토리북 분리

---
+++ src/components/common/Badge/NewBadge.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, StoryFn, StoryObj } from '@storybook/react';
+import { NewBadge } from '@/components/common/Badge';
+
+const meta: Meta<typeof NewBadge> = {
+  title: 'Components/Badge/New Badge',
+  component: NewBadge,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Template: StoryFn<typeof NewBadge> = (args) => <NewBadge {...args} />;
+
+export const Default = Template.bind({});
