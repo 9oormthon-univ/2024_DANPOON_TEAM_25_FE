b4422934bdd6a67983a3fbcbbfe2a83840ff2e4a
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-22 19:06:29 +0900

    [feat] InviteButton size 추가 구현

--- src/components/common/Button/InviteButton.stories.tsx
+++ src/components/common/Button/InviteButton.stories.tsx
@@ -6,11 +6,22 @@ const meta: Meta<typeof InviteButton> = {
   title: 'Components/Button/Invite Button',
   component: InviteButton,
   tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['medium', 'large'],
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const Template: StoryFn<typeof InviteButton> = (args) => <InviteButton />;
+const Template: StoryFn<typeof InviteButton> = (args) => (
+  <InviteButton {...args} />
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  size: 'medium',
+};
--- src/components/common/Button/InviteButton.tsx
+++ src/components/common/Button/InviteButton.tsx
@@ -1,9 +1,17 @@
-import { Button } from '@/components/common/Button/Button';
+import { Button, ButtonProps } from '@/components/common/Button/Button';
 import { Share2 } from 'lucide-react';
 
-export const InviteButton = () => (
-  <Button className="flex items-center px-4 py-2 gap-x-3 bg-blue-50 text-blue-800 rounded-lg cursor-pointer">
+interface InviteButtonProps extends BaseProps {
+  size?: 'medium' | 'large';
+}
+
+export const InviteButton = ({ size = 'medium' }: InviteButtonProps) => (
+  <Button
+    className={`flex items-center gap-x-3 bg-blue-50 text-blue-800 rounded-lg cursor-pointer ${
+      size === 'medium' ? 'px-4 py-2' : 'px-6 py-4 text-lg font-medium'
+    }`}
+  >
     <Share2 className="w-4 h-4 text-blue-800" />
-    초대하기
+    {size === 'medium' ? '초대하기' : '공유하기'}
   </Button>
 );
