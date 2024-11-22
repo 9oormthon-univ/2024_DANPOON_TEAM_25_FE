3217f26841d790fb74d966b8061c5b6a26720af9
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-23 04:17:34 +0900

    [style] hover 배경색 추가

--- src/components/common/Button/EditButton.tsx
+++ src/components/common/Button/EditButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/common/Button/Button';
 import { Pencil } from 'lucide-react';
 
 export const EditButton = () => (
-  <Button className="cursor-pointer">
+  <Button className="cursor-pointer p-2 rounded-full hover:bg-gray-100 transition-color">
     <Pencil className="w-3 h-3 text-gray-600" />
   </Button>
 );
