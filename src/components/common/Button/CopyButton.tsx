1dd52765de9b820cff26a15697811ab0543b49a3
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-22 19:06:10 +0900

    [feat] CopyButton 컴포넌트 및 스토리북 구현

---
+++ src/components/common/Button/CopyButton.tsx
@@ -0,0 +1,27 @@
+import { Button, ButtonProps } from '@/components/common/Button/Button';
+import { Copy } from 'lucide-react';
+
+export const CopyButton: React.FC<ButtonProps> = ({ children, ...props }) => {
+  const handleCopy = () => {
+    const copyCode = document.getElementById('copyTxt');
+
+    if (
+      copyCode instanceof HTMLInputElement ||
+      copyCode instanceof HTMLTextAreaElement
+    ) {
+      copyCode.select();
+      navigator.clipboard.writeText(copyCode.value);
+      alert('복사되었습니다!');
+    }
+  };
+
+  return (
+    <Button
+      className="flex items-center px-6 py-4 text-lg gap-x-3 font-medium text-blue-50 bg-blue-800 rounded-lg cursor-pointer"
+      onClick={handleCopy}
+    >
+      <Copy className="w-6 h-6 text-blue-50" />
+      코드 복사하기
+    </Button>
+  );
+};
