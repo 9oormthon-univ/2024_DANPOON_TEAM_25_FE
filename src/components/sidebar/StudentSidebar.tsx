9ec61c57502b1678ac42ff698eddb70c67c383e9
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-23 17:12:34 +0900

    [feat] Sidebar 탭 라우팅 구현

--- src/components/sidebar/StudentSidebar.tsx
+++ src/components/sidebar/StudentSidebar.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Sidebar } from './Sidebar';
 import { Book, PlusCircle, Settings } from 'lucide-react';
 
 export const StudentSidebar: React.FC = () => {
+  const [activeItem, setActiveItem] = useState('course');
   const menuItems = [
-    { id: 'course', text: '내 수업', icon: Book },
-    { id: 'register', text: '수업 등록', icon: PlusCircle },
-    { id: 'setting', text: '프로필', icon: Settings },
+    { id: 'course', text: '내 수업', icon: Book, route: '/student/courses' },
+    {
+      id: 'register',
+      text: '수업 등록',
+      icon: PlusCircle,
+      route: '/student/courses/register',
+    },
   ];
 
   return (
@@ -15,11 +20,13 @@ export const StudentSidebar: React.FC = () => {
         name: '학생 이름',
         avatar: {
           src: 'https://via.placeholder.com/150',
-          alt: '학생 프로필 이미지'
+          alt: '학생 프로필 이미지',
         },
-        subtitle: '환영합니다'
+        subtitle: '환영합니다',
       }}
       menuItems={menuItems}
+      activeItem={activeItem}
+      setActiveItem={setActiveItem}
       onMenuItemClick={(id) => console.log(`Student clicked ${id}`)}
     />
   );
--- src/components/sidebar/TeacherSidebar.tsx
+++ src/components/sidebar/TeacherSidebar.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Sidebar } from './Sidebar';
-import { Book, Briefcase, PlusCircle, Settings } from 'lucide-react';
+import { useRouter } from 'next/router';
+import { Book, BookOpenText, Briefcase, PlusCircle } from 'lucide-react';
 
 export const TeacherSidebar: React.FC = () => {
+  const [activeItem, setActiveItem] = useState('course');
+  const router = useRouter();
+
+  const isCoursePage =
+    router.pathname.startsWith('/teacher/courses/') &&
+    !router.pathname.startsWith('/teacher/courses/create');
+
   const menuItems = [
-    { id: 'course', text: '내 수업', icon: Book },
-    { id: 'register', text: '수업 등록', icon: PlusCircle },
-    { id: 'work-list', text: '워크 / 트레이닝 목록', icon: Briefcase },
-    { id: 'setting', text: '프로필', icon: Settings },
+    { id: 'course', text: '내 수업', icon: Book, route: '/teacher/courses' },
+    {
+      id: 'register',
+      text: '수업 등록',
+      icon: PlusCircle,
+      route: '/teacher/courses/create',
+    },
+    ...(isCoursePage
+      ? [
+          {
+            id: 'work-list',
+            text: '워크 목록',
+            icon: Briefcase,
+            route: '/teacher/courses/:courseId/works',
+          },
+          {
+            id: 'training-list',
+            text: '트레이닝 목록',
+            icon: BookOpenText,
+            route: '/teacher/courses/:courseId/trainings',
+          },
+        ]
+      : []),
   ];
 
   return (
@@ -21,6 +48,8 @@ export const TeacherSidebar: React.FC = () => {
         subtitle: '환영합니다',
       }}
       menuItems={menuItems}
+      activeItem={activeItem}
+      setActiveItem={setActiveItem}
       onMenuItemClick={(id) => console.log(`Teacher clicked ${id}`)}
     />
   );
