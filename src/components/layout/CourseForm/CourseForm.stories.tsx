24e973ee1ed77bdad77901ecdd585dddb15aeea3
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-23 10:07:12 +0900

    [feat] CourseForm props 적용

--- src/components/layout/CourseForm/CourseForm.stories.tsx
+++ src/components/layout/CourseForm/CourseForm.stories.tsx
@@ -1,15 +1,39 @@
-// import React from 'react';
-// import { Meta, StoryFn } from '@storybook/react';
-// import { CourseForm } from '@/components/layout/CourseForm/CourseForm';
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { CourseForm } from '@/components/layout/CourseForm/CourseForm';
+import { action } from '@storybook/addon-actions';
 
-// const meta: Meta<typeof CourseForm> = {
-//   title: 'Components/CourseForm',
-//   component: CourseForm,
-//   tags: ['autodocs'],
-// };
+const meta: Meta<typeof CourseForm> = {
+  title: 'Components/CourseForm',
+  component: CourseForm,
+  tags: ['autodocs'],
+  argTypes: {
+    courseName: { control: 'text', description: '수업 명' },
+    courseDescription: {
+      control: 'text',
+      description: '수업 설명',
+    },
+    setCourseName: {
+      action: 'setCourseName',
+    },
+    setCourseDescription: {
+      action: 'setCourseDescription',
+    },
+    className: {
+      control: 'text',
+    },
+  },
+};
 
-// export default meta;
+export default meta;
 
-// const Template: StoryFn<typeof CourseForm> = () => <CourseForm />;
+const Template: StoryFn<typeof CourseForm> = (args) => <CourseForm {...args} />;
 
-// export const Default = Template.bind({});
+export const Default = Template.bind({});
+Default.args = {
+  courseName: '수업 명',
+  setCourseName: action('setCourseName'),
+  courseDescription: '수업 설명',
+  setCourseDescription: action('setCourseDescription'),
+  className: '',
+};
