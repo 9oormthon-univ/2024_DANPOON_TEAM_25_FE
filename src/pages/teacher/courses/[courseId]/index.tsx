8241f74371213200bc63b8134f353033b3fb13f6
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-23 23:18:54 +0900

    [feat] task 목록 상단 task 생성 버튼 추가

--- src/pages/teacher/courses/[courseId]/index.tsx
+++ src/pages/teacher/courses/[courseId]/index.tsx
@@ -1,3 +1,4 @@
+import { AddButton } from '@/components/common/Button/AddButton';
 import { Card } from '@/components/common/Card/Card';
 import { SummaryCard } from '@/components/common/Card/SummaryCard';
 import { PageHeader } from '@/components/layout/PageHeader/PageHeader';
@@ -10,39 +11,55 @@ import { useState } from 'react';
 const CourseDetail = () => {
   const [activeTab, setActiveTab] = useState(0);
   return (
-     <>
-        <PageHeader
-          pageTitle="모두 함께 파이썬"
-          isDetailPage
-          role="teacher"
-          hasBackButton
-          hasSubtitle
-          subtitle="기초부터 시작하는 파이썬 프로그램"
-        />
-        <div className="my-8">
-          <div className="grid grid-cols-4 gap-8 mb-8">
-            <SummaryCard label="수강생" value="32명" icon={Users} />
-            <SummaryCard label="총 워크" value="12개" icon={Book} />
-            <SummaryCard label="완료율" value="78%" icon={Calendar} />
-            <SummaryCard label="트레이닝" value="6개" icon={CirclePlus} />
+    <>
+      <PageHeader
+        pageTitle="모두 함께 파이썬"
+        isDetailPage
+        role="teacher"
+        hasBackButton
+        hasSubtitle
+        subtitle="기초부터 시작하는 파이썬 프로그램"
+      />
+      <div className="my-8">
+        <div className="grid grid-cols-4 gap-8 mb-8">
+          <SummaryCard label="수강생" value="32명" icon={Users} />
+          <SummaryCard label="총 워크" value="12개" icon={Book} />
+          <SummaryCard label="완료율" value="78%" icon={Calendar} />
+          <SummaryCard label="트레이닝" value="6개" icon={CirclePlus} />
+        </div>
+      </div>
+
+      <Card>
+        <div className="p-6">
+          <WorkTrainingTabs activeTab={activeTab} onTabChange={setActiveTab} />
+          <div className="mt-6 space-y-4">
+            {activeTab === 0 && (
+              <>
+                <div className="flex justify-between">
+                  <h3 className="font-bold text-base pl-3">워크 목록</h3>
+                  <AddButton children="워크 추가" />
+                </div>
+                {taskData.map((task) => (
+                  <TaskItem key={task.id} {...task} />
+                ))}
+              </>
+            )}
+
+            {activeTab === 1 && (
+              <>
+                <div className="flex justify-between">
+                  <h3 className="font-bold text-base pl-3">트레이닝 목록</h3>
+                  <AddButton children="트레이닝 추가" />
+                </div>
+                {taskData.map((task) => (
+                  <TaskItem key={task.id} {...task} />
+                ))}
+              </>
+            )}
           </div>
         </div>
-
-        <Card>
-          <div className="p-6">
-            <WorkTrainingTabs
-              activeTab={activeTab}
-              onTabChange={setActiveTab}
-            />
-            <div className="mt-6 space-y-4">
-              {activeTab === 0 &&
-                taskData.map((task) => <TaskItem key={task.id} {...task} />)}
-              {activeTab === 1 &&
-                taskData.map((task) => <TaskItem key={task.id} {...task} />)}
-            </div>
-          </div>
-        </Card>
-      </>
+      </Card>
+    </>
   );
 };
 
